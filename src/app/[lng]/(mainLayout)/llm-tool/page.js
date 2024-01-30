a7334260f73ae264bdbca67e932dcb957d82086c
JamesWangDev <james.wang.dev712@gmail.com>
2024-01-30 17:59:09 +0000

    updated the left panel with x number of text boxe

--- src/app/[lng]/(mainLayout)/llm-tool/page.js
+++ src/app/[lng]/(mainLayout)/llm-tool/page.js
@@ -1,31 +1,38 @@
 "use client";
 
 import styles from "./llm-tool.module.scss";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useResizable } from "react-resizable-layout";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap"
 import { cn } from "./cn";
+import Btn from "@/Elements/Buttons/Btn"
+
+const texttypes = [
+  {
+    name: "Prompt Text",
+    value: 0
+  },
+  {
+    name: "Data Source",
+    value: 1
+  },
+  {
+    name: "Query Text",
+    value: 2
+  }
+]
+
+const placeHolderText = [
+  "Enter Prompt Text...",
+  "Enter Data Source...",
+  "Enter Query Text...",
+]
 
 const LLMTool = () => {
-  const {
-    isDragging: isTerminalDragging,
-    position: terminalH,
-    separatorProps: terminalDragBarProps,
-  } = useResizable({
-    axis: "y",
-    initial: 400,
-    min: 50,
-    reverse: false,
-  });
-  const {
-    isDragging: isTerminalDragging1,
-    position: terminalH1,
-    separatorProps: terminalDragBarProps1,
-  } = useResizable({
-    axis: "y",
-    initial: 650,
-    min: 50,
-    reverse: false,
-  });
+  const [dropdown1, setDropdown1] = useState(false);
+  const toggle1 = () => setDropdown1((prevState) => !prevState);
+  const [selectedTextType, setSelectedTextType] = useState("");
+  const [textBoxes, setTextBoxes] = useState([0, 1, 2])
   const {
     isDragging: isFileDragging,
     position: fileW,
@@ -36,46 +43,69 @@ const LLMTool = () => {
     min: 50,
   });
 
+  const handleAddTextBox = useCallback(() => {
+    if(selectedTextType === "") {
+      alert("Please select the Box type...")
+      return;
+    }
+    setTextBoxes((prev) => {
+      return [...prev, selectedTextType]
+    })
+  }, [selectedTextType, setSelectedTextType])
+
+  const handleRemoveTextBox = (index) => {
+    const update = textBoxes.filter((_, i) => i !== index)
+    setTextBoxes(update)
+  }
+
   return (
     <div className="d-flex justify-content-center">
       <div
         className={"flex flex-column w-75 overflow-hidden border border-black"}
       >
         <div className={"flex grow"}>
-          <div
-            className={"shrink-0 contents"}
-            style={{ width: fileW - 240 }}
-          >
-            <div className={"flex flex-column w-100"} style={{height: "700px",  width: fileW - 240 }}>
-              <div
-                className={"shrink-0 contents w-100"}
-                style={{ height: terminalH - 280 }}
-              >
-                  <textarea style={{width: "100%", height: "100%", padding: "8px", border: "none", resize: "none"}} placeholder="Prompt Text" />
-              </div>
-              <SampleSplitter
-                dir={"horizontal"}
-                isDragging={isTerminalDragging}
-                {...terminalDragBarProps}
-              />
-              <div className={"flex flex-column w-100"}>
-                <div
-                  className={"shrink-0 contents w-100"}
-                  style={{ height: terminalH1 - 430 }}
+          <div className={"shrink-0 contents"} style={{ width: fileW - 240 }}>
+            <div
+              className={"flex flex-column w-100"}
+              style={{ height: "700px", width: fileW - 240 }}
+            >
+              <Dropdown isOpen={dropdown1} toggle={toggle1}>
+                <DropdownToggle
+                  caret
+                  className="w-100 select-dropdown border border-black"
+                  type="button"
+                  size="sm"
                 >
-                    <textarea style={{width: "100%", height: "100%", padding: "8px", border: "none", resize: "none"}} placeholder="Data Source" />
-                </div>
-                <SampleSplitter
-                  dir={"horizontal"}
-                  isDragging={isTerminalDragging1}
-                  {...terminalDragBarProps1}
-                />
-                <div
-                  className={"shrink-0 contents"}
-                  style={{height: `${700 - terminalH - terminalH1 + 650}px`}}
-                >
-                    <textarea style={{width: "100%", height: "100%", padding: "8px", border: "none", resize: "none"}} placeholder="Query Text" />
-                </div>
+                  {texttypes[selectedTextType]
+                    ? texttypes[selectedTextType].name
+                    : "Select Text Box Type...."}
+                </DropdownToggle>
+                {
+                  <DropdownMenu className="dropdown-menu-end sm-dropdown-menu w-100">
+                    {texttypes.map((item, index) => (
+                        <DropdownItem
+                          id={`${item.name}${index}-1`}
+                          key={`${item.name}${index}-1`}
+                          onClick={() => setSelectedTextType(item.value)}
+                        >
+                          {item.name}
+                        </DropdownItem>
+                      ))}
+                  </DropdownMenu>
+                }
+              </Dropdown>
+              <Btn
+                  className='btn-sm w-100 border border-black mt-1 mb-1'
+                  title={'Add Text Box'}
+                  onClick={handleAddTextBox}>
+              </Btn>
+              <div className="overflow-auto">
+                {textBoxes.map((box, index) => (
+                  <div style={{position: "relative"}}>
+                    <textarea style={{resize: "none", width: "100%", height: "100px", padding: "8px", marginTop: index !==0 && "-7px"}} placeholder={placeHolderText[box]} />
+                    <span style={{position: "absolute", top: index !==0 ? "-8px": "0px", right: "6px", width: "10px", height: "10px", cursor: "pointer"}} onClick={() => handleRemoveTextBox(index)}>x</span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>

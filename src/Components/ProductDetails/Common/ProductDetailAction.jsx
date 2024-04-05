cafd20791d45ec77a2b9b2d0709bede17d08a031
JamesWangDev <james.wang.dev712@gmail.com>
2024-04-05 15:49:52 +0200

    updated the original chat box

--- src/Components/ProductDetails/Common/ProductDetailAction.jsx
+++ src/Components/ProductDetails/Common/ProductDetailAction.jsx
@@ -88,7 +88,7 @@ const ProductDetailAction = ({ productState, setProductState, extraOption }) => 
         ) : null}
       </div>
       <AddToCartButton productState={productState} isLoading={isLoading} addToCart={addToCart} buyNow={buyNow} extraOption={extraOption} />
-      <CustomModal modal={openChat} setModal={setOpenChat} classes={{modalBodyClass: "full-modal", modalClass: 'theme-modal modal-xl', title: productState?.product?.name }} fullscreen>
+      <CustomModal modal={openChat} setModal={setOpenChat} classes={{modalBodyClass: "full-modal", modalClass: 'theme-modal modal-xl', title: productState?.product?.name }}>
           {/* <iframe style={{width: "100%", height: "100%"}} src={`https://n8n.gpt-autopilot.com/index.php?smessage=${prompt || "normal"}`} title="Supermind chat box"></iframe> */}
           {productState?.product?.type == "superpower" ? <SuperpowerChatBox productData={productState.product} /> : <ChatBox productData={productState.product} />}
       </CustomModal>

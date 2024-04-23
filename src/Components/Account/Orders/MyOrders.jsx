20f98e90af267a4df7ce1e4e2e3dc2ad5b436dff
JamesWangDev <james.wang.dev712@gmail.com>
2024-04-23 15:48:30 +0200

    fixed the my supermind panel issue

--- src/Components/Account/Orders/MyOrders.jsx
+++ src/Components/Account/Orders/MyOrders.jsx
@@ -12,16 +12,17 @@ import { Col, Row } from 'reactstrap';
 import ProductBox1 from '@/Components/Common/ProductBox/ProductBox1/ProductBox1';
 import noProduct from '../../../../public/assets/svg/no-product.svg';
 import ProductSkeletonComponent from '@/Components/Common/SkeletonLoader/ProductSkeleton/ProductSkeletonComponent';
+import { AddToCartAPI } from '@/Utils/AxiosUtils/API';
 
 
 const MyOrders = () => {
   const [page, setPage] = useState(1);
   const { i18Lang } = useContext(I18NextContext);
   const { t } = useTranslation(i18Lang, 'common');
-  const { data, fetchStatus, isLoading, refetch } = useQuery([page], () => request({ url: myproducts, params: { page: page, paginate: 10 } }), {
+  const { data, fetchStatus, isLoading, refetch } = useQuery([page, AddToCartAPI], () => request({ url: AddToCartAPI, params: { page: page, paginate: 10 } }), {
     enabled: true,
     refetchOnWindowFocus: false,
-    refetchOnMount: false,
+    refetchOnMount: true,
     select: (res) => res?.data,
   });
   if (isLoading) return <Loader />;
@@ -32,11 +33,11 @@ const MyOrders = () => {
         <Row xxl={''} xl={3} lg={2} md={3} xs={2} className={`g-sm-4 g-3 product-list-section ${''}`}>
           <ProductSkeletonComponent item={40} />
         </Row>
-      ) : data?.data?.length > 0 ? (
+      ) : data?.items?.length > 0 ? (
         <Row xxl={''} xl={3} lg={2} md={3} xs={2} className={`g-sm-4 g-3 product-list-section ${''}`}>
-          {data?.data?.map((product, i) => (
+          {data?.items?.map((product, i) => (
             <Col key={i}>
-              <ProductBox1 imgUrl={product?.product_thumbnail} productDetail={{ ...product }} classObj={{ productBoxClass: 'product-box-3' }} />
+              <ProductBox1 imgUrl={product?.product?.product_thumbnail} productDetail={{ ...product?.product }} classObj={{ productBoxClass: 'product-box product-box-3' }} />
             </Col>
           ))}
         </Row>
@@ -53,7 +54,7 @@ const MyOrders = () => {
         />
       )}
 
-      {data?.data?.length > 0 && (
+      {data?.items?.length > 0 && (
         <nav className='custome-pagination'>
           <Pagination current_page={data?.current_page} total={data?.total} per_page={data?.per_page} setPage={setPage} />
         </nav>
